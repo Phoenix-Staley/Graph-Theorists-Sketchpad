4dc9a896e0c85c38ef3d0fbf04822f3b3303d523
phoenixstaley121@hotmail.com <phoenixstaley121@hotmail.com>
2025-02-14 15:48:56 -0800

    Add getNeighbors()

--- public/js/Node.js
+++ public/js/Node.js
@@ -8,4 +8,19 @@ export class Node {
         this.size = nodeSize;
         this.edges = [];
     }
+
+    getNeighbors() {
+        let neighbors = [];
+
+        for (let i = 0; i < this.edges.length; i++) {
+            const endpoints = edges[i].endpoints;
+            if (endpoints[0].name != this.name) {
+                neighbors.push(endpoints[0]);
+            } else {
+                neighbors.push(endpoints[1]);
+            }
+        }
+
+        return neighbors;
+    }
 }
